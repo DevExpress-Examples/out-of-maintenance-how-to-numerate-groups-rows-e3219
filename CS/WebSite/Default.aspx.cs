dfc9b7e6f77cbcb376b63e65d2ffaf03ed7f9531
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DevExpress.Web.ASPxGridView;
+using System.Data;
+
+public partial class _Default : System.Web.UI.Page {
+	private List<int> groupIndexes = new List<int>();
+	int rowInGroupNumber = 1;
+	bool isFirstDisplayedRow = true;
+
+	private bool IsGridUngrouped { get { return groupIndexes.Count == 0; } }
+
+	protected void grid_CustomColumnDisplayText(object sender, DevExpress.Web.ASPxGridView.ASPxGridViewColumnDisplayTextEventArgs e) {
+		if (e.Column.Name != "RowNumber")
+			return;
+
+		ASPxGridView g = sender as ASPxGridView;
+
+		if (IsGridUngrouped)
+			rowInGroupNumber = e.VisibleRowIndex + 1;
+		else {
+			if (isFirstDisplayedRow) {
+				rowInGroupNumber = e.VisibleRowIndex - GetParentGroupIndex(e.VisibleRowIndex);
+				isFirstDisplayedRow = false;
+			}
+			else {
+				if (IsRowIsFirstGroup(e.VisibleRowIndex))
+					rowInGroupNumber = 1;
+				else
+					rowInGroupNumber++;
+			}
+		}
+		e.Value = rowInGroupNumber;
+		e.DisplayText = rowInGroupNumber.ToString();
+	}
+	protected void grid_BeforeGetCallbackResult(object sender, EventArgs e) {
+		CollectGroupIndexes();
+	}
+
+	private void CollectGroupIndexes() {
+		groupIndexes.Clear();
+		for (int i = 0; i < grid.VisibleRowCount; i++) {
+			if (grid.IsGroupRow(i))
+				groupIndexes.Add(i);
+		}
+	}
+	private int GetParentGroupIndex(int index) {
+		return groupIndexes.FindLast(delegate(int i) { return i < index; });
+	}
+
+	private bool IsRowIsFirstGroup(int index) {
+		return grid.IsGroupRow(index - 1);
+	}
+}
---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,69 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Web
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports DevExpress.Web.ASPxGridView
+Imports System.Data
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+	Private groupIndexes As List(Of Integer) = New List(Of Integer)()
+	Private rowInGroupNumber As Integer = 1
+	Private isFirstDisplayedRow As Boolean = True
+	Private checkedIndex As Integer
+
+	Private ReadOnly Property IsGridUngrouped() As Boolean
+		Get
+			Return groupIndexes.Count = 0
+		End Get
+	End Property
+
+	Protected Sub grid_CustomColumnDisplayText(ByVal sender As Object, ByVal e As DevExpress.Web.ASPxGridView.ASPxGridViewColumnDisplayTextEventArgs)
+		If e.Column.Name <> "RowNumber" Then
+			Return
+		End If
+
+		Dim g As ASPxGridView = TryCast(sender, ASPxGridView)
+
+		If IsGridUngrouped Then
+			rowInGroupNumber = e.VisibleRowIndex + 1
+		Else
+			If isFirstDisplayedRow Then
+				rowInGroupNumber = e.VisibleRowIndex - GetParentGroupIndex(e.VisibleRowIndex)
+				isFirstDisplayedRow = False
+			Else
+				If IsRowIsFirstGroup(e.VisibleRowIndex) Then
+					rowInGroupNumber = 1
+				Else
+					rowInGroupNumber += 1
+				End If
+			End If
+		End If
+		e.Value = rowInGroupNumber
+		e.DisplayText = rowInGroupNumber.ToString()
+	End Sub
+	Protected Sub grid_BeforeGetCallbackResult(ByVal sender As Object, ByVal e As EventArgs)
+		CollectGroupIndexes()
+	End Sub
+
+	Private Sub CollectGroupIndexes()
+		groupIndexes.Clear()
+		For i As Integer = 0 To grid.VisibleRowCount - 1
+			If grid.IsGroupRow(i) Then
+				groupIndexes.Add(i)
+			End If
+		Next i
+	End Sub
+	Private Function GetParentGroupIndex(ByVal index As Integer) As Integer
+		checkedIndex = index
+		Return groupIndexes.FindLast(AddressOf CompareIndexex)
+	End Function
+	Private Function CompareIndexex(ByVal i As Integer) As Boolean
+		Return i < checkedIndex
+	End Function
+	Private Function IsRowIsFirstGroup(ByVal index As Integer) As Boolean
+		Return grid.IsGroupRow(index - 1)
+	End Function
+End Class
